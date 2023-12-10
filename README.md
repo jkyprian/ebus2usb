0ca804a4de6d57c73e58690b863b6b493d538951
Jan Eric Kyprianidis <jkyprian@gmail.com>
2023-12-10 16:40:09 +0100

    New version with STM32 microcontroller.

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
 # ebus2usb - 1TE DIN Rail eBUS to USB Type-C Interface
 
 eBUS is a serial 2-wire serial data-bus, often used in heating, solar energy, and other building automation systems. This project provides an interface to access an eBUS via USB.
-It uses a series transistor regulator circuit with a low IZT zener diode to extract the operating voltage from the eBUS, an ADuM1201 for the galvanic isolation, and a CH340N for the UART to USB conversion.
-Power consumption on the eBUS side is between 620uA (eBUS idle low/8V) and 920uA (eBUS idle high/24V), making the interface qualify as a class 0 device with respect to the eBUS specification.
-It has been designed to fit into an [CNMB/1/2](https://www.camdenboss.com/camden-boss/cnmb12-din-rail-module-box%2c-polycarbonate-enclosure%2c-industrial-iot-electronics-housing/c-23/p-18541) 1TE DIN rail enclosure by CAMDENBOSS. The interface is intended to be used with [ebusd](https://github.com/john30/ebusd/) and can be used as a replacement for an [eBUS Adapter v2](https://adapter.ebusd.eu/v2/).
+It uses a series transistor regulator circuit with a low IZT zener diode to extract the operating voltage from the eBUS, an ADuM1201 for the galvanic isolation, and a STM32 microcontroller for the UART to USB conversion.
+Power consumption on the eBUS side is between 475uA (eBUS low/8V) and 750uA (eBUS idle high/24V), making the interface qualify as a class 0 device with respect to the eBUS specification.
+It has been designed to fit into an [CNMB/1/2](https://www.camdenboss.com/camden-boss/cnmb12-din-rail-module-box%2c-polycarbonate-enclosure%2c-industrial-iot-electronics-housing/c-23/p-18541) 1TE DIN rail enclosure by CAMDENBOSS. The interface is intended to be used with [ebusd](https://github.com/john30/ebusd/). Note that the firmware is currently just a proof-of-concept and only implements a basic uart to USB bridge. Support for the ebusd enhanced protocol is planned for the near future.
 
-![Images](ebus2usb-img-v0.9.jpg)
+![Images](ebus2usb-img-v1.0.jpg)
 
 ## Schematic
 
@@ -21,29 +21,36 @@ When ordering at JLCPCB, remove the mouse bites and cut the boards yourself. Oth
 
 |Designator|Footprint|Quantity|Value|LCSC Part #
 |:---|:---|:---|:---|:---
-|C1, C2|C0805|2|10u|C15850
-|C3, C4, C5, C6, C7, C8|C0805|6|100n|C28233
-|D1, D2|SOT-23-3|2|BAV99|C2500
-|D3|SOT-123|1|5V6, Onsemi MMSZ4690*|C152307
-|D4|SOD-123|1|24V/1W, KDZVTR24B, MM1W24|C209605, C438336
-|D5|SOD-123|1|7V5/1W, KDZVTR7.5B, MM1W7V5|C209594, C382950
-|D6, D7, D8|D3.0mm|3||[CHANZON 3mm diffused LED](https://www.aliexpress.com/item/1936229691.html)
+|C1, C2, C7|C0805|3|10u|C15850
+|C10, C3, C4, C5, C6, C8, C9|C0603|7|100n|C14663
+|C11, C12|C0805|2|1u|C28323
+|D1, D2, D4|SOT-23-3|3|BAV99|C2500
+|D3|SOD-123|1|5V6, Onsemi MMSZ4690*|C152307
+|D5|SOD-123|1|24V/1W, KDZVTR24B, MM1W24|C209605, C438336
+|D6|SOD-123|1|7V5/1W, KDZVTR7.5B, MM1W7V5|C209594, C382950
+|D7|LED_0603|1|LED|C72043
+|D8, D9, D10|D3.0mm|3||[CHANZON 3mm diffused LED](https://www.aliexpress.com/item/1936229691.html)
 |J1|MSTBA_2,5_2-G-5,08|1|MSTBA 2,5/2-G-5,08|C3697
-|J2, J5|Molex_PicoBlade_53047-0210|2|Molex 53047-0210|C114130
-|J3, J6|Molex_PicoBlade_53047-0410|2|Molex 53047-0410|C504989
-|J4|HRO_TYPE-C-31-M-12|1|HRO TYPE-C-31-M-12|C165948
+|J2, J6|Molex_PicoBlade_53047-0210|2|Molex 53047-0210|C114130
+|J3, J7|Molex_PicoBlade_53047-0410|2|Molex 53047-0410|C504989
+|J5|HRO_TYPE-C-31-M-12|1|HRO TYPE-C-31-M-12|C165948
+|J8|PinHeader_1x02_P1.27mm|1|Harwin M50-3530242|C2881948
 |Q1, Q2|SOT-23-3|2|BC817-40, S9013J3|C52801, C6749
-|R1|R0805|1|0R|C17477
-|R2, R3, R4, R11|R0805|4|100K|C149504
-|R5|R0805|1|4K7|C17673
-|R6|R0805|1|270K|C17589
-|R7|R0805|1|68K|C17801
-|R8|R0805|1|2M|C26112
-|R9, R10|R0805|2|5K1|C27834
-|R12, R13|R0805|2|22K|C17560
+|R1, R2, R3|R0603|3|100K|C25803
+|R10|R0603|1|47K|C25819
+|R12, R13|R0603|2|5K1|C23186
+|R14|R0805|1|100K|C149504
+|R15, R16|R0805|2|15K|C17475
+|R4|R0603|1|4K7|C23162
+|R5|R0603|1|300K|C23024
+|R6|R0603|1|51K|C23196
+|R7|R0603|1|2M|C22976
+|R8|R0603|1|10K|C25804
+|R9|R0603|1|0R|C21189
 |U1|SO08|1|AZV3002, TSX3702|C5123396, C2970396
 |U2|SO08|1|ADuM1201, π122U31|C9669, C471590
-|U3|SO08|1|CH340N, CH330N|C2977777
-|U4|SOT-23-6L|1|SRV05-4|C85364
+|U5|SOT-23-6L|1|SRV05-4|C85364
+|U6|SOT-23-3|1|XC6206P332MR|C5446
+|U7|LQFP-32|1|STM32L412KBT6, STM32F042K6T6|C529438, C69216
 
 \* Low IZT of 50uA is required. Other manufacturers than Onsemi may not qualify. For example, MMSZ4690 from Jiangsu Changjing Elec. Tech. is not recommened.
---
+++ firmware/Core/Src/main.c
@@ -0,0 +1,309 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "usb_device.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "usbd_cdc_if.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+int rx_head = 0;
+int rx_tail = 0;
+uint8_t RX_BUF[64];
+int tx_head = 0;
+int tx_tail = 0;
+uint8_t TX_BUF[64];
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+static void MX_GPIO_Init(void);
+static void MX_USART2_UART_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART2_UART_Init();
+  MX_USB_DEVICE_Init();
+  /* USER CODE BEGIN 2 */
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  HAL_GPIO_WritePin(LED_GPIO_Port, LED_Pin, GPIO_PIN_SET);
+  while (1)
+  {
+    //HAL_GPIO_TogglePin(LED_GPIO_Port, LEDPin);
+    if (LL_USART_IsActiveFlag_ORE(USART2)) {
+      LL_USART_ClearFlag_ORE(USART2);
+    }
+    if (LL_USART_IsActiveFlag_RXNE(USART2)) {
+      RX_BUF[rx_head] = LL_USART_ReceiveData8(USART2);
+      rx_head = (rx_head + 1) & 0x3F;
+    }
+    if (LL_USART_IsActiveFlag_TXE(USART2) && (tx_tail != tx_head)) {
+      LL_USART_ClearFlag_TC(USART2);
+      LL_USART_TransmitData8(USART2, TX_BUF[tx_tail]);
+      tx_tail = (tx_tail + 1) & 0x3F;
+    }
+
+    if (rx_tail != rx_head) {
+      if (CDC_Transmit_FS(&RX_BUF[rx_tail], 1) == USBD_OK) {
+            rx_tail = (rx_tail + 1) & 0x3F;
+      }
+    }
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  LL_FLASH_SetLatency(LL_FLASH_LATENCY_4);
+  while(LL_FLASH_GetLatency()!= LL_FLASH_LATENCY_4)
+  {
+  }
+  LL_PWR_SetRegulVoltageScaling(LL_PWR_REGU_VOLTAGE_SCALE1);
+  while (LL_PWR_IsActiveFlag_VOS() != 0)
+  {
+  }
+  LL_RCC_HSI_Enable();
+
+   /* Wait till HSI is ready */
+  while(LL_RCC_HSI_IsReady() != 1)
+  {
+
+  }
+  LL_RCC_HSI_SetCalibTrimming(64);
+  LL_RCC_MSI_Enable();
+
+   /* Wait till MSI is ready */
+  while(LL_RCC_MSI_IsReady() != 1)
+  {
+
+  }
+  LL_RCC_MSI_EnableRangeSelection();
+  LL_RCC_MSI_SetRange(LL_RCC_MSIRANGE_11);
+  LL_RCC_MSI_SetCalibTrimming(0);
+  LL_RCC_PLL_ConfigDomain_SYS(LL_RCC_PLLSOURCE_HSI, LL_RCC_PLLM_DIV_1, 10, LL_RCC_PLLR_DIV_2);
+  LL_RCC_PLL_EnableDomain_SYS();
+  LL_RCC_PLL_Enable();
+
+   /* Wait till PLL is ready */
+  while(LL_RCC_PLL_IsReady() != 1)
+  {
+
+  }
+  LL_RCC_SetSysClkSource(LL_RCC_SYS_CLKSOURCE_PLL);
+
+   /* Wait till System clock is ready */
+  while(LL_RCC_GetSysClkSource() != LL_RCC_SYS_CLKSOURCE_STATUS_PLL)
+  {
+
+  }
+  LL_RCC_SetAHBPrescaler(LL_RCC_SYSCLK_DIV_1);
+  LL_RCC_SetAPB1Prescaler(LL_RCC_APB1_DIV_1);
+  LL_RCC_SetAPB2Prescaler(LL_RCC_APB2_DIV_1);
+  LL_SetSystemCoreClock(80000000);
+
+   /* Update the time base */
+  if (HAL_InitTick (TICK_INT_PRIORITY) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief USART2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_USART2_UART_Init(void)
+{
+
+  /* USER CODE BEGIN USART2_Init 0 */
+
+  /* USER CODE END USART2_Init 0 */
+
+  LL_USART_InitTypeDef USART_InitStruct = {0};
+
+  LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  LL_RCC_SetUSARTClockSource(LL_RCC_USART2_CLKSOURCE_PCLK1);
+
+  /* Peripheral clock enable */
+  LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_USART2);
+
+  LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOA);
+  /**USART2 GPIO Configuration
+  PA2   ------> USART2_TX
+  PA3   ------> USART2_RX
+  */
+  GPIO_InitStruct.Pin = LL_GPIO_PIN_2|LL_GPIO_PIN_3;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
+  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_VERY_HIGH;
+  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+  GPIO_InitStruct.Alternate = LL_GPIO_AF_7;
+  LL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /* USER CODE BEGIN USART2_Init 1 */
+
+  /* USER CODE END USART2_Init 1 */
+  USART_InitStruct.BaudRate = 2400;
+  USART_InitStruct.DataWidth = LL_USART_DATAWIDTH_8B;
+  USART_InitStruct.StopBits = LL_USART_STOPBITS_1;
+  USART_InitStruct.Parity = LL_USART_PARITY_NONE;
+  USART_InitStruct.TransferDirection = LL_USART_DIRECTION_TX_RX;
+  USART_InitStruct.HardwareFlowControl = LL_USART_HWCONTROL_NONE;
+  USART_InitStruct.OverSampling = LL_USART_OVERSAMPLING_16;
+  LL_USART_Init(USART2, &USART_InitStruct);
+  LL_USART_ConfigAsyncMode(USART2);
+  LL_USART_Enable(USART2);
+  /* USER CODE BEGIN USART2_Init 2 */
+
+  /* USER CODE END USART2_Init 2 */
+
+}
+
+/**
+  * @brief GPIO Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_GPIO_Init(void)
+{
+  LL_GPIO_InitTypeDef GPIO_InitStruct = {0};
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
+
+  /* GPIO Ports Clock Enable */
+  LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOA);
+  LL_AHB2_GRP1_EnableClock(LL_AHB2_GRP1_PERIPH_GPIOB);
+
+  /**/
+  LL_GPIO_ResetOutputPin(GPIOA, LED_Pin|TP3_Pin);
+
+  /**/
+  GPIO_InitStruct.Pin = LED_Pin|TP3_Pin;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_OUTPUT;
+  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_NO;
+  LL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
+}
+
+/* USER CODE BEGIN 4 */
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
